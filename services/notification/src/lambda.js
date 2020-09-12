08a00ec1886b4a6f889c9ac2af3b9298b9c342cc
marija <marija_grozdanic@yahoo.com>
2020-09-12 15:03:56 +0100

    add tf files for  sns, sqs, subscription creation on AWS

--- services/notification/src/lambda.js
+++ services/notification/src/lambda.js
@@ -23,17 +23,3 @@ exports.handler = async (event, context) => {
     };
   }
 };
-
-var LambdaForwarder = require("aws-lambda-ses-forwarder");
-
-exports.handler = function(event, context, callback) {
-    var overrides = {
-        config: {
-            fromEmail: process.env.EMAIL_FROM,
-            emailBucket: process.env.EMAIL_BUCKET_NAME,
-            emailKeyPrefix: process.env.EMAIL_BUCKET_PATH,
-            forwardMapping: JSON.parse(process.env.EMAIL_MAPPING)
-        }
-    };
-    LambdaForwarder.handler(event, context, callback, overrides);
-};
