5184b35e66a1211f7ceed93c9026ef866c0f1137
marija <marija_grozdanic@yahoo.com>
2020-09-12 14:53:52 +0100

    add tf files for  sns, sqs, subscription creation on AWS

--- services/notification/deploy/main.tf
+++ services/notification/deploy/main.tf
@@ -70,9 +70,9 @@ POLICY
 # ---------------------------------------------------------------------------------------------------------------------
 # SNS SUBSCRIPTION
 # ---------------------------------------------------------------------------------------------------------------------
-
-resource "aws_sns_topic_subscription" "sqs_target" {
-  topic_arn = aws_sns_topic.sns_topic.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.queue.arn
-}
+//
+//resource "aws_sns_topic_subscription" "sqs_target" {
+//  topic_arn = aws_sns_topic.sns_topic.arn
+//  protocol  = "sqs"
+//  endpoint  = aws_sqs_queue.queue.arn
+//}
