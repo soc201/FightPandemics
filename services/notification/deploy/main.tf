c8c93446eaaa489c791649aad00857af0d555a81
marija <marija_grozdanic@yahoo.com>
2020-09-16 16:01:17 +0100

    output route values

--- services/notification/deploy/main.tf
+++ services/notification/deploy/main.tf
@@ -125,12 +125,13 @@ data "aws_route53_zone" "selected" {
 }
 
 resource "aws_route53_record" "www" {
-  zone_id = "${data.aws_route53_zone.selected.zone_id}"
-  name    = "www.${data.aws_route53_zone.selected.name}"
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = data.aws_route53_zone.selected.name
   type    = "A"
   ttl     = "300"
   records = ["10.0.0.1"]
 }
+
 
 
 
